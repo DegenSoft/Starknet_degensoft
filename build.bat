9f91f8a68a3c9b1174363c4f10523683e714cc4e
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2023-07-03 01:03:28 +0100

    build.bat fix

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ pyinstaller --onefile --windowed --clean --name starknet_degensoft ^
     --add-binary="c:\Users\Administrator\AppData\Local\Programs\Python\Python311\Lib\site-packages\libcrypto_c_exports.dll;." ^
     --add-binary="c:\Users\Administrator\AppData\Local\Programs\Python\Python311\Lib\site-packages\starknet_py\cairo\deprecated_parse\cairo.ebnf;starknet_py\cairo\deprecated_parse" ^
     --add-binary="starknet_degensoft\abi\starkgate.json;starknet_degensoft\abi" ^
-gui.py
+main.py
 cp config.json dist\
