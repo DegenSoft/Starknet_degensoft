5e8f66bffa33acde124cb516bc4543d1c640568e
Degensoft-vlad <140105174+Degensoft-vlad@users.noreply.github.com>
2023-10-07 00:51:03 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,6 @@ pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --onefile --windowed --clean --name starknet_degensoft ^
     --add-binary="venv\Lib\site-packages\libcrypto_c_exports.dll;." ^
-    --add-binary="venv\Lib\site-packages\starknet_py\cairo\deprecated_parse\cairo.ebnf;starknet_py\cairo\deprecated_parse" ^
     --add-binary="starknet_degensoft\abi\starkgate.json;starknet_degensoft\abi" ^
 main.py
 copy config.json dist\
