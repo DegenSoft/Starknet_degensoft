248fb8a6bba149410f6d2df2838e7048115a32de
Degensoft-vlad <Degensoft_vlad@proton.me>
2023-10-06 22:18:39 +0300

    build config

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ call venv\Scripts\Activate
 pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --onefile --windowed --clean --name starknet_degensoft ^
-    --add-binary="c:\Users\Administrator\AppData\Local\Programs\Python\Python311\Lib\site-packages\libcrypto_c_exports.dll;." ^
-    --add-binary="c:\Users\Administrator\AppData\Local\Programs\Python\Python311\Lib\site-packages\starknet_py\cairo\deprecated_parse\cairo.ebnf;starknet_py\cairo\deprecated_parse" ^
+    --add-binary="c:\Users\degensoft\AppData\Local\Programs\Python\Python311\Lib\site-packages\libcrypto_c_exports.dll;." ^
     --add-binary="starknet_degensoft\abi\starkgate.json;starknet_degensoft\abi" ^
 main.py
-cp config.json dist\
+copy config.json dist\
+pause
