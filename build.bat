84e929327bc335554738cf3aaa64178d0e7b14ce
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2023-10-30 19:55:53 +0100

    Update build.bat
    pyinstaller==6.1.0 version fix

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 call venv\Scripts\Activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install pyinstaller
+pip install pyinstaller==6.1.0
 pyinstaller --onefile --windowed --clean --name starknet_degensoft ^
     --add-binary="venv\Lib\site-packages\libcrypto_c_exports.dll;." ^
     --add-binary="starknet_degensoft\abi\starkgate.json;starknet_degensoft\abi" ^
