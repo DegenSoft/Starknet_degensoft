f046760c6149696bc55234a2e94c2d766a1a9d95
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2023-11-28 22:00:02 +0000

    dmail call fix

--- starknet_degensoft/starknet_dmail.py
+++ starknet_degensoft/starknet_dmail.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import random
+from hashlib import sha256
 from starknet_degensoft.starknet_nft import BaseNft
 from starknet_degensoft.utils import resource_path
 
@@ -9,17 +10,9 @@ class StarknetDmail(BaseNft):
     project_name = 'DMail'
     _contract_address = '0x0454f0bd015e730e5adbb4f080b075fdbf55654ff41ee336203aa2e1ac4d4309'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        with open(resource_path(os.path.join('..', 'starknet_degensoft', 'files', 'words.txt'))) as f:
-            self.words = [line.strip() for line in f]
-
     async def mint_async(self):
-        email, subject = self.generate_random_email()
-        print(email, subject)
-
-    def generate_random_email(self):
-        email = random.choice(self.words) + random.choice(['@gmail.com', '@yandex.com', '@dmail.ai'])
-        subject_length = random.randint(1, 25)
-        subject = ' '.join(random.sample(self.words, subject_length))
-        return email, subject
+        email_address = sha256(str(1e10 * random.random()).encode()).hexdigest()
+        theme = sha256(str(1e10 * random.random()).encode()).hexdigest()
+        dmail_call = self.contract.functions['transaction'].prepare(email_address[0:31], theme[0:31])
+        invoke = await self.account.sign_invoke_transaction([dmail_call], auto_estimate=True)
+        return await self.account.client.send_transaction(invoke)
