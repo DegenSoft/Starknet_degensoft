344436074fd4eb4dab012bafa17c26be2693fb42
Arseniy Sluchevskiy <arseniy.sluchevskiy@gmail.com>
2023-12-01 00:51:56 +0000

    update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -3,6 +3,5 @@ pip3 install virtualenv
 source venv/bin/activate
 pip install -r requirements.txt
 pip install pyinstaller
-
-pyinstaller --onefile --windowed --clean --name starknet_degensoft --add-binary="starknet_degensoft/abi/starkgate.json:starknet_degensoft/abi" --add-binary="venv/lib/python3.11/site-packages/libcrypto_c_exports.dylib:." main.py
+pyinstaller --noconfirm starknet_degensoft.spec
 cp config.json dist/config.json
